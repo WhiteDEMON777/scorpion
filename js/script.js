f05db50ae19bce6f7d281ce506c8f9d76fe55b4f
Darilov.Dmitrij <whitedemon2092@gmail.com>
2023-02-27 23:04:08 +0400

    add

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 
 $(document).ready(function(){
-    setTimeout(function() {
-        $(document).scrollTop(0)
-    },0);
+    const swiper = new Swiper('.reviews__slider', {
+        loop: true,
+        pagination: {
+            el: '.reviews__slider-pagination',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1.2,
+              spaceBetween: 15
+            },
+            // when window width is >= 480px
+            576: {
+              slidesPerView: 2.2,
+            },
+            // when window width is >= 640px
+            768: {
+              slidesPerView: 3.08,
+              spaceBetween: 30
+            }
+          }
+    });
+    // setTimeout(function() {
+    //     $(document).scrollTop(0)
+    // },0);
     $('.modalbox').fancybox({
         touch: false
     });
@@ -81,51 +103,51 @@ $(document).ready(function(){
 });
 
 
-$(window).on('load', function () {
+// $(window).on('load', function () {
    
-    if($(window).width() > 1024){
-       var wow = new WOW({
-        animateClass: 'wow animate__animated',
-        mobile:false,
-        offset: 100
-    })
+//     if($(window).width() > 1024){
+//        var wow = new WOW({
+//         animateClass: 'wow animate__animated',
+//         mobile:false,
+//         offset: 100
+//     })
     
-    }
-    if($(window).width() > 1024) {
-        $('.nav').addClass('wow animate__animated animate__fadeInDown').css({ animationDelay: '0.6s' });
-        $('.header__title').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.header__subtitle').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1s' });
-        $('.header__text').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.2s' });
-        $('.header__btn').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.4s' });
-        $('.info__content').eq(0).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.info__content').eq(1).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.info__content').eq(2).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1s' });
-        $('.company__content').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.company__img').addClass('wow animate__animated animate__fadeIn').css({ animationDelay: '0.6s' });
-        $('.services__title').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.services__subtitle').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.services__item').eq(0).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.services__item').eq(1).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.services__item').eq(2).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.2s' });
-        $('.services__item').eq(3).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.services__item').eq(4).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.services__item').eq(5).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.2s' });
-        $('.services__item').eq(6).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.services__item').eq(7).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.services__item').eq(8).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.2s' });
-        $('.stages__title').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.stages__item').eq(0).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.stages__item').eq(1).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1s' });
-        $('.stages__item').eq(2).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.stages__item').eq(3).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.experience__content').eq(1).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.experience__content').eq(2).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.appeal__title').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
-        $('.appeal__subtitle').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
-        $('.appeal__content').addClass('wow animate__animated animate__fadeIn').css({ animationDelay: '1s' });
-        $('.footer__logo').addClass('wow animate__animated animate__fadeInLeft').css({ animationDelay: '0.6s' });
-        $('.footer__content').addClass('wow animate__animated animate__fadeIn').css({ animationDelay: '0.6s' });
-        $('.footer__contacts').addClass('wow animate__animated animate__fadeInRight').css({ animationDelay: '0.6s' });
-        wow.init();
-    }
-});
+//     }
+//     if($(window).width() > 1024) {
+//         $('.nav').addClass('wow animate__animated animate__fadeInDown').css({ animationDelay: '0.6s' });
+//         $('.header__title').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.header__subtitle').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1s' });
+//         $('.header__text').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.2s' });
+//         $('.header__btn').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.4s' });
+//         $('.info__content').eq(0).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.info__content').eq(1).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.info__content').eq(2).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1s' });
+//         $('.company__content').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.company__img').addClass('wow animate__animated animate__fadeIn').css({ animationDelay: '0.6s' });
+//         $('.services__title').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.services__subtitle').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.services__item').eq(0).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.services__item').eq(1).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.services__item').eq(2).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.2s' });
+//         $('.services__item').eq(3).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.services__item').eq(4).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.services__item').eq(5).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.2s' });
+//         $('.services__item').eq(6).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.services__item').eq(7).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.services__item').eq(8).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1.2s' });
+//         $('.stages__title').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.stages__item').eq(0).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.stages__item').eq(1).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '1s' });
+//         $('.stages__item').eq(2).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.stages__item').eq(3).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.experience__content').eq(1).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.experience__content').eq(2).addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.appeal__title').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.6s' });
+//         $('.appeal__subtitle').addClass('wow animate__animated animate__fadeInUp').css({ animationDelay: '0.8s' });
+//         $('.appeal__content').addClass('wow animate__animated animate__fadeIn').css({ animationDelay: '1s' });
+//         $('.footer__logo').addClass('wow animate__animated animate__fadeInLeft').css({ animationDelay: '0.6s' });
+//         $('.footer__content').addClass('wow animate__animated animate__fadeIn').css({ animationDelay: '0.6s' });
+//         $('.footer__contacts').addClass('wow animate__animated animate__fadeInRight').css({ animationDelay: '0.6s' });
+//         wow.init();
+//     }
+// });
